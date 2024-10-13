59fd1775be9f9f1518a4eccfd0f5a05628938136
hieutrinhduy <143570193+hieutrinhduy@users.noreply.github.com>
2024-10-13 20:31:00 +0700

    1013

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,10 +1,38 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.EventSystems;
 
 public class SceneChanger : MonoBehaviour
 {
     [SerializeField] private Button skipButton;
+    private RectTransform skipButtonRect;
+    private Vector3 initialPosition;
+    private bool isOffScreen = false;
+
+    private void Start()
+    {
+        skipButtonRect = skipButton.GetComponent<RectTransform>();
+        initialPosition = skipButtonRect.anchoredPosition;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !IsPointerOverUIElement())
+        {
+            if (!isOffScreen)
+            {
+                skipButtonRect.DOAnchorPosY(Screen.height, 0.2f).SetEase(Ease.InOutQuad);
+            }
+            else
+            {
+                skipButtonRect.DOAnchorPosY(initialPosition.y, 0.2f).SetEase(Ease.InOutQuad);
+            }
+            isOffScreen = !isOffScreen;
+        }
+    }
+
     public void ChangeScene(string sceneName)
     {
         ASyncLoader.Instance.IsChangeToNextScene = true;
@@ -12,4 +40,8 @@ public class SceneChanger : MonoBehaviour
         SceneManager.LoadScene(sceneName);
     }
 
+    private bool IsPointerOverUIElement()
+    {
+        return EventSystem.current.IsPointerOverGameObject();
+    }
 }
