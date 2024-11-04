330919664ecbbc129442b9395349ec86d6347f6b
hieutrinhduy <143570193+hieutrinhduy@users.noreply.github.com>
2024-11-04 14:52:37 +0700

    0411

--- Assets/CutSceneTrigger.cs
+++ Assets/CutSceneTrigger.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CutSceneTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private string LevelName;
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(LevelName);
+        }
     }
 }
